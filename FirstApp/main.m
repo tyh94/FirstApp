93c812689d1b3d4a8826a9f0237b237e0f384e7d
admin <admin@admins-imac-2.local>
2016-07-05 15:49:54 +0600

    Initial Commit

---
+++ FirstApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by admin on 05/07/16.
+//  Copyright © 2016 tanya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
